65cd4a6b9e2616fa5f583ee511cd3d18268d90c9
Bartosz Kepka <bartoszkepka1999@gmail.com>
2021-04-09 21:07:14 +0200

    Show temp

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const Navbar: React.FC = () => {
+  const [response, setResponse] = useState<any>({});
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("http://localhost:5000/building", { method: "GET" })
+        .then((response) => response.json())
+        .then((data) => {
+          setResponse(data);
+        });
+    }, 1000);
+  }, []);
+
+  let hours =
+    new Date(response.time).getUTCHours() < 10
+      ? `0${new Date(response.time).getUTCHours()}`
+      : new Date(response.time).getUTCHours();
+  let minutes =
+    new Date(response.time).getUTCMinutes() < 10
+      ? `0${new Date(response.time).getUTCMinutes()}`
+      : new Date(response.time).getUTCMinutes();
+
   return (
     <div className="navbar">
       <div className="navbar__box">
         <p>Time:</p>
-        <p className="navbar__time">21:37</p>
+        {Object.keys(response).length != 0 ? (
+          <p className="navbar__time">{`${hours}:${minutes}`}</p>
+        ) : (
+          <p className="navbar__time">Loading simulation time...</p>
+        )}
       </div>
       <div className="navbar__box">
         <p>Cloud cover:</p>
