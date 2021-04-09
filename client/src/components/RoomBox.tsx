1ef2e33fa21f0cf4855f0d8a15c22a1d1e5953bf
Bartosz Kepka <bartoszkepka1999@gmail.com>
2021-04-10 00:28:26 +0200

    Show target temperature in rooms

--- client/src/components/RoomBox.tsx
+++ client/src/components/RoomBox.tsx
@@ -13,23 +13,33 @@ export const RoomBox: React.FC<props> = ({ name, temperature, heating }) => {
   return (
     <div className="room-box">
       <p className="room-box__name">{name}</p>
-      <span style={{ display: "flex", alignItems: "center" }}>
-        <Icon color="primary">
-          <WhatshotOutlinedIcon />
-        </Icon>
-        <span className="room-box__temperature">
-          Temperatura: {`${temperature}°C`}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "flex-start",
+          flexDirection: "column",
+        }}
+      >
+        <span style={{ display: "flex", alignItems: "center" }}>
+          <span className="room-box__temperature">
+            Temperature: {`${temperature}°C`}
+          </span>
+          {heating ? (
+            <Icon style={{ transform: "rotate(-90deg)" }} color="secondary">
+              <ArrowRightAltOutlinedIcon />
+            </Icon>
+          ) : (
+            <Icon style={{ transform: "rotate(90deg)" }} color="primary">
+              <ArrowRightAltOutlinedIcon />
+            </Icon>
+          )}
         </span>
-        {heating ? (
-          <Icon style={{ transform: "rotate(-90deg)" }} color="secondary">
-            <ArrowRightAltOutlinedIcon />
-          </Icon>
-        ) : (
-          <Icon style={{ transform: "rotate(90deg)" }} color="primary">
-            <ArrowRightAltOutlinedIcon />
-          </Icon>
-        )}
-      </span>
+        <span style={{ marginTop: "10px" }}>
+          <span className="room-box__temperature">
+            Target temperature: {`${temperature}°C`}
+          </span>
+        </span>
+      </div>
     </div>
   );
 };
