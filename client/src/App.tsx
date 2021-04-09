0958527f0529c13b0fdf801b2940e21e09aee75c
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-04-09 18:02:55 +0200

    Add functionality to preview building

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@material-ui/core";
 import Header from "./components/Header";
 import "./App.scss";
@@ -6,13 +6,15 @@ import "./App.scss";
 function App() {
   const [response, setResponse] = useState<string>("");
 
-  const onButtonClick = () => {
-    fetch("http://localhost:5000/users", { method: "GET" })
-      .then((response) => response.json())
-      .then((data) => {
-        setResponse(JSON.stringify(data));
-      });
-  };
+  useEffect(() => {
+    setInterval(() => {
+      fetch("http://localhost:5000/building", { method: "GET" })
+        .then((response) => response.json())
+        .then((data) => {
+          setResponse(JSON.stringify(data));
+        });
+    }, 1000);
+  });
 
   return (
     <div className="App">
@@ -20,9 +22,6 @@ function App() {
         <Header hint="hint of a header" divider={true}>
           Header
         </Header>
-        <Button variant="contained" color="primary" onClick={onButtonClick}>
-          Click to fetch
-        </Button>
         <p style={{ backgroundColor: "rgba(0.5, 0.5, 0.5, 0.2" }}>{response}</p>
       </header>
     </div>
