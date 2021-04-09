ff3a829688b6a00bbe487ccd7697275d452c5361
Jan Konieczny <jan_xboxlive@outlook.com>
2021-04-09 19:47:39 +0200

    add room model

---
+++ server/src/models/room.model.ts
@@ -0,0 +1,8 @@
+export class Room {
+
+  public current_temperature: number | null = null;
+  public target_temperature: number |null=null;
+  public heating_power: number | null=null
+  public heating_rate_per_tick: number;
+
+}
