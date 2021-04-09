f52bbf0877f22f57fa382ec6e6cdde51500569cc
Jan Konieczny <jan_xboxlive@outlook.com>
2021-04-10 01:52:08 +0200

    add power consumption loop

--- server/src/models/building.model.ts
+++ server/src/models/building.model.ts
@@ -43,8 +43,9 @@ export class Building {
   public powerExchange: PowerExchange = new PowerExchange();
 
   public getConsumption() {
-
-    this.rooms.forEach(room => { (room.is_heated === true) ? powerConsumption += room.heating_power : powerConsumption -= room.heating_power});
+    let powerConsumption = 0;
+    this.rooms.forEach(room => { (room.is_heated === true) ? powerConsumption += room.heating_power : "" });
+    console.log("powerConsumption: ", powerConsumption);
     return powerConsumption;
   }
 
@@ -57,16 +58,12 @@ export class Building {
    // const panelEfficiency = this.panels.getEfficiency(newTime, this.sensors.clearSkyRatio);
     
     this.heatRooms();
-    console.log(this.sensors.outside.temperature);
+    // console.log(this.sensors.outside.temperature);
     
-    console.table({
-      room1: [],
-   
-    });
     console.table([0,1,2,3,4,5,6].map(id=> ({is_heated:this.rooms[id].is_heated, current_temperature:this.rooms[id].current_temperature, 
-      target_temperature: this.rooms[id].target_temperature, differenceCheck: this.differenceCheck(this.rooms[id]), newTime})))
+      target_temperature: this.rooms[id].target_temperature, differenceCheck: this.differenceCheck(this.rooms[id]), newTime})));
 
-    console.log(this.differenceCheck(this.rooms[0]));
+    // console.log(this.differenceCheck(this.rooms[0]));
 
     // console.table({
     //   time: newTime.toISOString(),
@@ -87,6 +84,10 @@ export class Building {
   public heatRooms() {
     this.rooms.forEach(room => {this.differenceCheck(room) ? room.is_heated = true : room.is_heated = false});
     this.rooms.forEach(room => {room.is_heated ? room.current_temperature += 1/6 : room.current_temperature += this.temperatureDecrease(this.sensors.outside.temperature) })
+
+    while(this.getConsumption() > 10){
+      this.rooms[this.getRoomWithMinDifference().id].is_heated = false; 
+    }
   
   }
 
@@ -126,11 +127,26 @@ export class Building {
 
   return temperatureDecrease;
   }
+  public getRoomWithMinDifference(){
+    let differenceArray=[]
+    let heatedRooms=[];
+    this.rooms.forEach(room => {room.is_heated ? heatedRooms.push(room) : "" });
 
+
+    for(let i=0; i< heatedRooms.length ;i++){
+      differenceArray.push({'id':heatedRooms[i].id,
+      'difference':heatedRooms[i].current_temperature - heatedRooms[i].target_temperature})
+    }
+
+    return differenceArray.reduce(function(prev, curr) {
+      return prev.difference < curr.difference ? prev : curr;
+    })
+
+  }
 }
 
 // starting values here
 const defaultBuilding: Building = new Building();
-let powerConsumption = 0;
+
 
 export default defaultBuilding;
