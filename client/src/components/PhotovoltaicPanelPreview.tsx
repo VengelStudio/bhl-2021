650cdbcec0a0d811aac96be9e59e0c0d29f55836
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-04-10 04:28:09 +0200

    Add working chart

--- client/src/components/PhotovoltaicPanelPreview.tsx
+++ client/src/components/PhotovoltaicPanelPreview.tsx
@@ -12,47 +12,59 @@ export interface PhotovoltaicPanelPreviewProps {
 }
 
 interface ChartData {
-  argument: number;
+  argument: any;
   value: number;
 }
 
 export const PhotovoltaicPanelPreview: React.FC<PhotovoltaicPanelPreviewProps> = ({
   response,
 }) => {
-  const [calculatedData, setCalculatedData] = useState<ChartData[]>([
-    { argument: 1, value: 10 },
-    { argument: 2, value: 20 },
-    { argument: 3, value: 50 },
-  ]);
+  const [calculatedData, setCalculatedData] = useState<ChartData[]>([]);
 
   useEffect(() => {
-    const lastId =
-      calculatedData.length > 0
-        ? calculatedData[calculatedData.length - 1].argument
-        : 0;
     setCalculatedData([
       ...calculatedData,
       {
-        argument: lastId + 1,
+        argument: new Date(response.time),
         value: response.building.panels.efficiency,
       },
     ]);
     console.log(response.building.panels.efficiency);
   }, [response]);
 
+  const ValueLabel = (props: any) => {
+    const { text } = props;
+    return <ValueAxis.Label {...props} text={`${text} kWh`} />;
+  };
+
+  const format = () => (tick: any) => {
+    let hours =
+      new Date(tick).getUTCHours() < 10
+        ? `0${new Date(tick).getUTCHours()}`
+        : new Date(tick).getUTCHours();
+    let minutes =
+      new Date(tick).getUTCMinutes() < 10
+        ? `0${new Date(tick).getUTCMinutes()}`
+        : new Date(tick).getUTCMinutes();
+
+    return `${hours}:${minutes}`;
+  };
+
   return (
     <div>
-      <span>{`Power production: ${response.building.panels.efficiency} kWh`}</span>
-      <Chart data={calculatedData}>
-        <ArgumentAxis />
-        <ValueAxis />
+      <span>{`Current power production: ${response.building.panels.efficiency} kWh`}</span>
+      <Paper style={{ marginTop: "16px" }}>
+        <Chart height={200} data={calculatedData}>
+          <ArgumentAxis tickFormat={format} />
+          <ValueAxis labelComponent={ValueLabel} />
 
-        <LineSeries
-          color="#000000"
-          valueField="value"
-          argumentField="argument"
-        />
-      </Chart>
+          <LineSeries
+            color="#000000"
+            valueField="value"
+            argumentField="argument"
+          />
+        </Chart>
+      </Paper>
     </div>
   );
 };
