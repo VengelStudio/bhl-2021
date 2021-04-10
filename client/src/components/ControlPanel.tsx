e2aaa4c304f26c0ad78e76f712eb4748b6e6a906
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-04-10 02:52:49 +0200

    Improve saving modes

--- client/src/components/ControlPanel.tsx
+++ client/src/components/ControlPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -7,6 +7,7 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export interface ControlPanelProps {
   value: "a" | "b" | "c" | "d";
@@ -17,7 +18,12 @@ export const ControlPanel: React.FC<ControlPanelProps> = ({
   value,
   onChange,
 }) => {
+  const [selectedOption, setSelectedOption] = useState<"a" | "b" | "c" | "d">(
+    value
+  );
+
   const handleChange = (event: any) => {
+    setSelectedOption(event.target.value);
     onChange(event.target.value);
   };
 
@@ -26,11 +32,16 @@ export const ControlPanel: React.FC<ControlPanelProps> = ({
       <h2>Select mode</h2>
       <div className="control-panel__switches">
         <FormControl component="fieldset">
-          <RadioGroup name="gender1" value={value} onChange={handleChange}>
+          <RadioGroup
+            name="gender1"
+            value={selectedOption}
+            onChange={handleChange}
+          >
             <div className="control-panel__cards">
               <Card>
                 <CardContent>
                   <FormControlLabel
+                    style={{ minHeight: "42px" }}
                     value="a"
                     control={<Radio />}
                     label="Mode A"
